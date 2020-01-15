16a41d3501641f1e011c75b4f6d1c3e32e307114
gabsrodriguez <gmederos95@gmail.com>
2020-01-15 20:04:59 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -15,3 +15,72 @@ db = SQLAlchemy()
 #             "username": self.username,
 #             "email": self.email
 #         }
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    zip = db.Column(db.Integer(20), nullable=False)
+    
+
+     def __repr__(self):
+         return '<Users %r>' % self.firstname
+
+     def serialize(self):
+         return {
+             "firstname": self.firstname,
+             "lastname": self.lastname
+             "email": self.email,
+             "password": self.password,
+             "zip": self.zip
+         }
+
+class Objects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    objectname = db.Column(db.String, nullable=False)
+    recycle = db.Column(db.String, nullable=False)
+    reuse = db.Column(db.String, nullable=False)
+    reduce = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+         return '<Objects %r>' % self.objectname
+
+     def serialize(self):
+         return {
+             "id": self.id,
+             "objectname": self.objectname
+             "recycle": self.recycle,
+             "reuse": self.reuse,
+             "reduce": self.reduce
+         }
+    
+class ResourceCenters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    centername = db.Column(db.String, nullable=False)
+    address = db.Column(db.Integer, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    zip = db.Column(db.Integer, nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
+    phonenumber = db.Column(db.integer, nullable=False)
+    website = db.Column(db.String, nullable=True)
+    type = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+         return '<ResourceCenters %r>' % self.firstname
+
+     def serialize(self):
+         return {
+             "cetername": self.centername,
+             "address": self.address,
+             "city": self.city,
+             "state": self.state,
+             "zip": self.zip,
+             "hours": self.hours,
+             "phonenumber" self.phonenumber,
+             "website": self.website,
+             "type": self.type
+         }
+
